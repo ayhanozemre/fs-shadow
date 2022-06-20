07e3247444ab0ea8a51d60f099ccadf3f81159f2
ayhan.ozemre <ayhan.ozemre@gmail.com>
2022-06-20 15:25:39 +0300

    stage1

--- cmd/main.go
+++ cmd/main.go
@@ -1,20 +1,71 @@
 package main
 
 import (
-	"github.com/ayhanozemre/fs-shadow/watcher"
-	log "github.com/sirupsen/logrus"
+	"fmt"
+	"github.com/rjeczalik/notify"
+	"golang.org/x/sys/unix"
+	"log"
 )
 
 func main() {
-	tw, err := watcher.NewFSPathWatcher("/tmp/fs-shadow-test")
+	//tw, err := watcher.NewFSPathWatcher("/home/wade/Desktop/TransferChain")
 	//tw, err := watcher.NewVirtualPathWatcher("/home/wade/Desktop/TransferChain")
 
-	if err == nil {
-		tw.PrintTree("INIT TREE")
-		done := make(chan bool)
-		<-done
-	} else {
-		log.Panic(err)
+	c := make(chan notify.EventInfo, 1)
+	go func() {
+		moves := make(map[uint32]struct {
+			From   string
+			To     string
+			Rename string
+		})
+		for {
+			ei := <-c
+			cookie := ei.Sys().(*unix.InotifyEvent).Cookie
+			info := moves[cookie]
+			switch ei.Event() {
+			case notify.InMovedFrom:
+				info.From = ei.Path()
+			case notify.InMovedTo:
+				info.To = ei.Path()
+			case notify.Rename:
+				info.Rename = ei.Path()
+			}
+
+			moves[cookie] = info
+
+			if cookie != 0 && info.From != "" && info.To != "" {
+				log.Println("File:", info.From, "was renamed to", info.To)
+				delete(moves, cookie)
+			} else if cookie != 0 && info.From != "" && info.Rename != "" {
+				log.Println("File:", info.From, "moved to another destination")
+			} else {
+				fmt.Println("X", ei)
+			}
+		}
+	}()
+
+	err := notify.Watch("/tmp/fs-shadow-test/...",
+		c,
+		notify.All,
+		notify.InMovedFrom,
+		notify.InMovedTo)
+	if err != nil {
+		log.Fatal(err)
 	}
-	tw.Close()
+	defer notify.Stop(c)
+
+	done := make(chan bool)
+	<-done
+	/*
+		if err == nil {
+			tw.PrintTree("INIT TREE")
+			done := make(chan bool)
+			<-done
+		} else {
+			logrus.Panic(err)
+		}
+		tw.Close()
+
+	*/
+
 }
